82c029a908d75bfaeefbab4e27d922a82e9117c0
tosnitichok407 <tosnitichok@gmail.com>
2026-08-10 15:05:28 +0700

    refactor: add database code

---
+++ config/database.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = "localhost";
+$dbname = "kpi_management_system";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
